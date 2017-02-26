3101ce8cd3daac12b2839544a85318356ef2ac7c
UHS <umair.h@live.co.uk>
2017-02-26 20:20:54 +0000

    Added Custom Class for ProductsCollectionViewCell

---
+++ UniversalApp/Controller/Cell/ProductsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductsCollectionViewCell.swift
+//  UniversalApp
+//
+//  Created by UHS on 26/02/2017.
+//  Copyright © 2017 Apkia Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ProductsCollectionViewCell: UICollectionViewCell {
+    
+}
