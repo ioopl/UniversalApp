8b5411b0a5f8382a5b805e6da889250091f51197
UHS <umair.h@live.co.uk>
2017-02-27 23:34:18 +0000

    Unit Tests for Products Search API URL

--- UniversalAppTests/UniversalAppTests.swift
+++ UniversalAppTests/UniversalAppTests.swift
@@ -10,27 +10,33 @@ import XCTest
 @testable import UniversalApp
 
 class UniversalAppTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    var timeout:TimeInterval = 30.00
+
+    func testProductsAPIURL() {
+        let URL = NSURL(string: "https://api.johnlewis.com/v1/products/search?q=dishwasher&key=Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb&pageSize=20")!
+        let expc = expectation(description: "Test Products Search API URL")
+        let session = URLSession.shared
+        let task = session.dataTask(with: URL as URL) { data, response, error in
+
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+
+            if let HTTPResponse = response as? HTTPURLResponse {
+                XCTAssertEqual(HTTPResponse.statusCode, 200, "HTTP response status code should be 200")
+            } else {
+                XCTFail("Response was not NSHTTPURLResponse")
+            }
+            expc.fulfill()
+        }
+        task.resume()
+
+        waitForExpectations(timeout: timeout) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                XCTFail("There is an Error")
+            }
+            task.cancel()
         }
     }
-    
 }
