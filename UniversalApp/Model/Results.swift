e5c822a527a81bd1bf61d2f53501e59dfa915827
UHS <umair.h@live.co.uk>
2017-12-14 22:50:49 +0000

    Results Object added as a Struct to hold API response.

---
+++ UniversalApp/Model/Results.swift
@@ -0,0 +1,32 @@
+//
+//  Product.swift
+//  UniversalApp
+//
+//  Created by UHS on 26/02/2017.
+//  Copyright © 2017 Apkia Technologies. All rights reserved.
+//
+
+import Foundation
+
+struct Results {
+    // MARK: - Variables
+    var image = String()
+    var id = String()
+    var name = String()
+    var artist = String()
+    var url = String()
+    
+    init(dictionary : [[String:Any]]) {
+        for result in dictionary {
+            name = result["name"] as! String
+            artist = result["artist"] as! String
+            url = result["url"] as! String
+            let imagesArray = result["image"] as! NSArray
+            // Get the value from index 1
+            let imageMediumArray = imagesArray[1]
+            guard let imageDictionary = imageMediumArray as? [String : AnyObject] else { return }
+            let imageMedium = imageDictionary["#text"] as! String
+            image = imageMedium
+        }
+    }
+}
