9e7a49b54e66226ab8b884644179590fb4ebd313
UHS <umair.h@live.co.uk>
2017-12-15 10:02:08 +0000

    DetailViewController added as a Secondary view and linked with associated UIViewController Swift file.

---
+++ UniversalApp/View/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  UniversalApp
+//
+//  Created by UHS on 15/12/2017.
+//  Copyright © 2017 Apkia Technologies. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
