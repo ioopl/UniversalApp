5630ed654e34a3451ef806a7d785530a1625a8fe
UHS <umair.h@live.co.uk>
2017-12-15 10:11:35 +0000

    Segue Push Navigation added in Storyboard.

--- UniversalApp/Code/Helpers/Constants/Constant.swift
+++ UniversalApp/Code/Helpers/Constants/Constant.swift
@@ -9,5 +9,5 @@
 struct Constant {
     static var APIKey: String = "42b1c939f4743c1795620262dc138cd4"
     static var APIURL: String = "http://ws.audioscrobbler.com/2.0/?method=track.search"
-    static var segueIdentifierMainController: String = "segueIdentifierMainController"
+    static var segueIdentifierMainController: String = "segueFromMainViewToDetailView"
 }
